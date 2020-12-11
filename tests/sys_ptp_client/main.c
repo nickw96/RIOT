ac77033306fa6959c13d9c010510d7a3296420b0
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2020-12-11 21:37:32 +0100

    tests/sys_ptp: enable STM32 specific debug output
    If stm32_eth is used, the PPS output of the Ethernet peripheral is enabled.

--- tests/sys_ptp_client/main.c
+++ tests/sys_ptp_client/main.c
@@ -21,13 +21,24 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "kernel_defines.h"
+#include "net/gnrc.h"
+#include "net/gnrc/pktdump.h"
+#include "periph/gpio.h"
+#include "periph_cpu.h"
 #include "shell.h"
 #include "shell_commands.h"
 
-#include "net/gnrc/pktdump.h"
-#include "net/gnrc.h"
-
 #define MAIN_QUEUE_SIZE     (8)
+#define _QUOTE(x)           #x
+#define QUOTE(x)            _QUOTE(x)
+
+#ifndef PPS_PIN
+#define PPS_PIN     GPIO_PIN(PORT_G, 8)
+#endif
+#ifndef PPS_FREQ
+#define PPS_FREQ    1
+#endif
 
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
@@ -43,6 +54,11 @@ int main(void)
          "Connect to a PTP server and use the \"ptp\" shell command to verify\n"
          "correct synchronization\n"
          "\n");
+
+#if IS_USED(MODULE_STM32_ETH)
+    puts("Enabling STM32 PTP-PPS signal at " QUOTE(PPS_FREQ) " Hz");
+    stm32_eth_ptp_enable_pps(PPS_PIN, PPS_FREQ);
+#endif
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 

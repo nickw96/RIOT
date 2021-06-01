34d646926790762a3eb950554180b0481788ead0
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2021-06-01 21:31:52 +0200

    boards/rpi-pico: add the Raspberry Pi Pico
    Co-authored-by: Fabian Hüßler <fabian.huessler@st.ovgu.de>

---
+++ boards/rpi-pico/include/periph_conf.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2021 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup         boards_rpi_pico
+ * @{
+ *
+ * @file
+ * @brief           Configuration of CPU peripherals for the Raspberry Pi Pico
+ * @author          Marian Buschsieweke <marian.buschsieweke@ovgu.de>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#include <stdint.h>
+
+#include "cpu.h"
+#include "periph_cpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */
